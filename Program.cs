5fb6840f48a68bc718c7b32d99acd1dc0bc33bf3
skclusive <skclusive@outlook.com>
2021-08-13 11:43:35 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+var services = new ServiceCollection();
+
+services.TryAddEnumerable(ServiceDescriptor.Scoped(typeof(IProcessor<int>), typeof(IntProcessor)));
+// Fix: ordering open generic above concretes
+services.TryAddEnumerable(ServiceDescriptor.Scoped(typeof(IProcessor<>), typeof(AnyProcessor<>)));
+
+var serviceProvider = services.BuildServiceProvider();
+
+using var scope = serviceProvider.CreateAsyncScope();
+
+// bug is reproducible only when below line enabled
+var processor = scope.ServiceProvider.GetService<IProcessor<int>>();
+
+var processors = scope.ServiceProvider.GetService<IEnumerable<IProcessor<int>>>() ?? Enumerable.Empty<IProcessor<int>>();
+
+// bug?: prints "IntProcessor -- IntProcessor" instead of IntProcessor -- AnyProcessor`1 if line 17 commented.
+Console.WriteLine(string.Join(" -- ", processors.Select(p => p.GetType().Name)));
+
+interface IProcessor<T> { }
+
+record AnyProcessor<T> : IProcessor<T>;
+
+record IntProcessor : IProcessor<int>;
+
